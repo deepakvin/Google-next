a02d0586d88df97f6cfa4c1c9cffb268b42ab7e2
deepakvin <deepakchaurasiya0321@gmail.com>
2024-04-06 11:26:37 +0530

    add body section

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+     /** @type {import('next').NextConfig} */
+    const nextConfig = {
+      reactStrictMode: true,
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'upload.wikimedia.org',
+            
+            pathname: '**',
+          },
+          
+        ],
+      },
+    }
+    
+    export default  nextConfig
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import HomeHeader from "@/components/HomeHeader";
-
+import HomeSearch from "@/components/HomeSearch";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="text-red-500">
+    <div>
       <HomeHeader />
+      <div className="flex  flex-col items-center mt-24">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+          alt="google logo"
+          width={300}
+          height={100}
+          priority
+          style={{width:'auto'}}
+        />
+        <HomeSearch />
+         
+        
+      </div>
 
     </div>
   );
